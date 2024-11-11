8e7add42a55cc17c238762cf11e867eae32dbe2c
Andrea Alice <andrea.alice@remira.com>
2024-11-11 17:02:32 +0100

    add foreach

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+List<string> wordsToGuess = new List<string>
+{
+    "Arance",
+    "Elefante",
+    "Albero",
+    "Zaino"
+
+};
+var validCharacters = new Regex("^[A-Z]$");
+int attempts = 10;
+List<string> letters = new();
+Random random = new Random();
+int wordIndex = random.Next(wordsToGuess.Count);
+int missingChars = 0; 
+string wordToGuess = wordsToGuess[wordIndex].ToUpper();
+foreach (char character in wordToGuess)
+{
+    string letter = character.ToString();
+    if (letters.Contains(letter))
+    {
+        Console.Write(letter);
+        missingChars--;
+    }
+    else
+    {
+        Console.Write("_ ");
+        missingChars++;
+    }
+}
